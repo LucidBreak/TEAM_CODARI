236ca6b6e2a0d27b0a1b5c9cbb4119cc923e2dcc
LucidBreak <chico501@naver.com>
2024-11-28 15:26:56 +0900

    Initial Commit

---
+++ CODARI/CODARIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CODARIApp.swift
+//  CODARI
+//
+//  Created by 김준서 on 2024/11/28.
+//
+
+import SwiftUI
+
+@main
+struct CODARIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CODARI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CODARI
+//
+//  Created by 김준서 on 2024/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("CODARI_COMMIT_TEST")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
